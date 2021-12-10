28243d4d24fb16d3bccda4d8c02f3ff4588966b6
DJKhouw <dejink@gmail.com>
2021-12-10 20:21:06 +0100

    Final solution

--- day03/day03_rs/src/main.rs
+++ day03/day03_rs/src/main.rs
@@ -8,6 +8,8 @@ fn main() {
         .collect::<Vec<_>>();
     let part1 = solve1(&input, 12);
     println!("Part 1: {}", part1);
+    let part2 = solve2(&input, 12);
+    println!("Part 2: {}", part2);
 }
 pub fn get_inverse(n: u32, bits: usize) -> u32 {
     n ^ (2_u32.pow(bits.try_into().unwrap()) - 1)
@@ -21,9 +23,9 @@ pub fn get_bit_at(n: u32, pos: usize) -> bool {
     }
 }
 
-pub fn most_common_bits(input: &[u32], bits: usize) -> Vec<bool> {
+pub fn common_bits(input: &[u32], bits: usize, most: bool) -> Vec<bool> {
     let halfway = input.len() / 2;
-    let mut ones = vec![0; bits.into()];
+    let mut ones = vec![0; bits];
     for val in input {
         for pos in 1..=bits {
             if get_bit_at(*val, pos) {
@@ -33,16 +35,28 @@ pub fn most_common_bits(input: &[u32], bits: usize) -> Vec<bool> {
     }
     let mut result = Vec::with_capacity(bits);
     for n in ones.iter().take(bits) {
-        if *n >= halfway {
-            result.push(true);
-        } else {
+        if *n > halfway {
+            if most {
+                result.push(true);
+            } else {
+                result.push(false);
+            }
+        } else if *n == halfway && input.len() % 2 == 0 {
+            if most {
+                result.push(true);
+            } else {
+                result.push(false)
+            }
+        } else if most {
             result.push(false);
+        } else {
+            result.push(true);
         }
     }
     result
 }
 pub fn solve1(input: &[u32], bits: usize) -> u32 {
-    let most_common = most_common_bits(input, bits);
+    let most_common = common_bits(input, bits, true);
     let gamma_string = most_common
         .iter()
         .map(|b| if *b { '1' } else { '0' })
@@ -55,45 +69,23 @@ pub fn solve1(input: &[u32], bits: usize) -> u32 {
 }
 pub fn reduce(input: &[u32], bits: usize, most_common: bool) -> u32 {
     let mut result = input.to_vec();
-    println!("Values: {:?}", &result);
-    let mut mcb = most_common_bits(&result, bits);
+    let mut cb = common_bits(&result, bits, most_common);
     let mut temp = Vec::new();
     for pos in (1..=bits).rev() {
-        println!("[reduce] position {}", &pos);
-        println!(
-            "MCB: {}",
-            mcb.iter()
-                .rev()
-                .map(|b| if *b { '1' } else { '0' })
-                .collect::<String>()
-        );
         for n in result.iter() {
-            if most_common {
-                if get_bit_at(*n, pos) == mcb[pos - 1] {
-                    println!(
-                        "Position {}, value: {}, bit: {}, mcb: {}",
-                        &pos,
-                        *n,
-                        get_bit_at(*n, pos),
-                        &mcb[pos - 1]
-                    );
-                    temp.push(*n);
-                    continue;
-                }
-            } else if get_bit_at(*n, pos) != mcb[pos - 1] {
+            if get_bit_at(*n, pos) == cb[pos - 1] {
                 temp.push(*n);
                 continue;
             }
         }
+
         if temp.len() == 1 {
             return temp[0];
         }
-        println!("Found {} numbers: {:?}", temp.len(), &temp);
         result.clear();
         result.append(&mut temp);
-        mcb = most_common_bits(&result, bits);
+        cb = common_bits(&result, bits, most_common);
     }
-
     0
 }
 pub fn solve2(input: &[u32], bits: usize) -> u32 {
@@ -106,7 +98,27 @@ pub fn solve2(input: &[u32], bits: usize) -> u32 {
 #[cfg(test)]
 mod tests {
     use super::*;
-
+    #[test]
+    fn test_most_common() {
+        let sequence = [30, 22, 23, 21, 28, 16, 25];
+        let mcb = common_bits(&sequence, 5, true);
+        println!("most common: {:?}", &mcb);
+        assert!(!mcb[0]);
+        assert!(!mcb[1]);
+        assert!(mcb[2]);
+        assert!(!mcb[3]);
+        assert!(mcb[4]);
+    }
+    #[test]
+    fn test_least_common() {
+        let sequence = [30, 22, 23, 21, 28, 16, 25];
+        let lcb = common_bits(&sequence, 5, false);
+        assert!(lcb[0]);
+        assert!(lcb[1]);
+        assert!(!lcb[2]);
+        assert!(lcb[3]);
+        assert!(!lcb[4]);
+    }
     #[test]
     fn test_inverse() {
         assert_eq!(get_inverse(1, 5), 30);
@@ -115,12 +127,11 @@ mod tests {
 
     #[test]
     fn test_get_bit_at() {
-        let most_common = vec![false, true, true, false, true];
-        assert_eq!(get_bit_at(4, 1), false);
-        assert_eq!(get_bit_at(4, 2), false);
-        assert_eq!(get_bit_at(4, 3), true);
-        assert_eq!(get_bit_at(4, 4), false);
-        assert_eq!(get_bit_at(4, 5), false);
+        assert!(!get_bit_at(4, 1));
+        assert!(!get_bit_at(4, 2));
+        assert!(get_bit_at(4, 3));
+        assert!(!get_bit_at(4, 4));
+        assert!(!get_bit_at(4, 5));
     }
     #[test]
     fn test_solve1() {
@@ -136,18 +147,6 @@ mod tests {
     }
 
     #[test]
-    fn test_oxygen_generator_rating() {
-        let input = vec![
-            "00100", "11110", "10110", "10111", "10101", "01111", "00111", "11100", "10000",
-            "11001", "00010", "01010",
-        ]
-        .iter()
-        .map(|l| u32::from_str_radix(l, 2).unwrap())
-        .collect::<Vec<_>>();
-        assert_eq!(solve2(&input, 5), 230);
-    }
-
-    #[test]
     fn test_most_common_bits() {
         let input = vec![
             "00100", "11110", "10110", "10111", "10101", "01111", "00111", "11100", "10000",
@@ -156,7 +155,8 @@ mod tests {
         .iter()
         .map(|l| u32::from_str_radix(l, 2).unwrap())
         .collect::<Vec<_>>();
-        let most_common = most_common_bits(&input, 5);
+        println!("input: {:?}", &input);
+        let most_common = common_bits(&input, 5, true);
         println!("most common: {:?}", &most_common);
         assert!(!most_common[0]);
         assert!(most_common[1]);
@@ -173,13 +173,19 @@ mod tests {
         .iter()
         .map(|l| u32::from_str_radix(l, 2).unwrap())
         .collect::<Vec<_>>();
-        let most_common = most_common_bits(&input, 5);
         assert_eq!(reduce(&input, 5, true), 23);
-        // fails, because mcb from 2nd iteration is 11111 instead of 10100
+        assert_eq!(reduce(&input, 5, false), 10);
     }
 
     #[test]
-    fn divide() {
-        println!("{}", (7_usize / 2_usize));
+    fn test_solve2() {
+        let input = vec![
+            "00100", "11110", "10110", "10111", "10101", "01111", "00111", "11100", "10000",
+            "11001", "00010", "01010",
+        ]
+        .iter()
+        .map(|l| u32::from_str_radix(l, 2).unwrap())
+        .collect::<Vec<_>>();
+        assert_eq!(solve2(&input, 5), 230);
     }
 }
